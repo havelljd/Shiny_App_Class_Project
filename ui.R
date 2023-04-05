a092c3671f9dddbede73c52b6f9d458e2392acc9
madik637 <125617829+madik637@users.noreply.github.com>
2023-04-04 18:14:05 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,49 @@
-
-
 ui <- fluidPage(
-  br(),
-  
-  fluidRow(
-    column(width=6, offset=6),
-    h1('hello world')
-  )
-)
 
-ui
+    fluidRow(
+      h1('Page title')
+    ),
+    fluidRow(
+      column(width = 6,
+             wellPanel(
+               p('Hello world')
+             )
+             ),
+      column(width = 6,
+             wellPanel(
+               p('Panel 2')
+             )
+             )
+    ),
+    tabsetPanel(
+      tabPanel(title = 'Inputs',
+               wellPanel(
+              textInput(inputId = 'my_input',label = 'Input',width = '200px',placeholder = 'Enter text here')
+               )),
+      tabPanel(title = 'Outputs',
+               wellPanel(
+                 plotlyOutput(outputId = 'my_output')
+               ))
+    )
+
+  )
+
+
+
+mytheme <- bs_theme(version = 5,
+                    bootswatch = 'sandstone',
+                    bg = '#000',
+                    fg = '#FFF')
+
+  ui <- navbarPage(title = 'Patent analytics',
+                   theme = mytheme,
+                   tabPanel(title = 'Home',
+
+                            wellPanel(
+                              p('Some text here')
+                            )
+                   ),
+                   tabPanel(title = 'Charts',
+                   )
+
+  )
