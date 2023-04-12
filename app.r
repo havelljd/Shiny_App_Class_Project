5ee01484b41383015574679e9862dc5919ed38ab
clarawcullen <125617926+clarawcullen@users.noreply.github.com>
2023-04-12 15:57:42 -0600

    Update app.r

--- app.r
+++ app.r
@@ -11,6 +11,7 @@ library(tidyverse)
 library(data.table)
 library(feather)
 
+library(DT)
 
 
 
@@ -28,18 +29,22 @@ library(feather)
 
 # upload the real cpc codes -- probably need to change file path to be transferable
 load("~/Desktop/strat/Module3/shiny/unique_cpc_group.Rdata")
-# class(unique_cpc_group)
-# structure(unique_cpc_group)
-# is.list(unique_cpc_group)
 unique_patent_codes <- unique(unique_cpc_group)
+unique_patent_codes <- unique_patent_codes[grepl("B60L|Y02", unique_patent_codes, ignore.case = TRUE)]
+## delete when we want full data, not filtered
+
+
+
+#upload smaller data for testing
+small_data <- fread("~/Desktop/strat/Module3/Data/small_data.csv")
 
 #load patent data ### set working directory
-patent <- fread("~/Desktop/strat/Module3/Data/g_patent_2012_2021.csv")
-application<- fread("~/Desktop/strat/Module3/Data/g_application_2012_2021.csv")
-assignee <- fread("~/Desktop/strat/Module3/Data/g_assignee_disambiguated_2012_2021.csv")
-term <-  fread("~/Desktop/strat/Module3/Data/g_us_term_of_grant_2012_2021.csv")
-cpc <- fread("~/Desktop/strat/Module3/Data/g_cpc_current_2012_2021.csv")
-location <- fread("~/Desktop/strat/Module3/Data/g_location_disambiguated_2012_2021.csv")
+# patent <- fread("~/Desktop/strat/Module3/Data/g_patent_2012_2021.csv")
+# application<- fread("~/Desktop/strat/Module3/Data/g_application_2012_2021.csv")
+# assignee <- fread("~/Desktop/strat/Module3/Data/g_assignee_disambiguated_2012_2021.csv")
+# term <-  fread("~/Desktop/strat/Module3/Data/g_us_term_of_grant_2012_2021.csv")
+# cpc <- fread("~/Desktop/strat/Module3/Data/g_cpc_current_2012_2021.csv")
+# location <- fread("~/Desktop/strat/Module3/Data/g_location_disambiguated_2012_2021.csv")
 
 
 #david's data
@@ -75,7 +80,8 @@ ui <- navbarPage(
     )
   ),
   
-  
+#i think when we use "patent_codes" it is when david used "market_cpcs_input" -- so i chnaged to his for consistency
+    
   # Define the competition analysis page
   tabPanel(
     title = 'Competition Analysis',   # Name the analysis page
@@ -83,20 +89,19 @@ ui <- navbarPage(
       column(
         width = 4,
         h4('Enter your search criteria:'),
-        selectInput(inputId = 'patent_codes', label = 'Patent Codes:', choices = unique_patent_codes, multiple = TRUE),
-        #selectInput(inputId = 'patent_codes', label = 'Patent Codes:', value = ''),
+        selectInput(inputId = 'market_cpcs_input', label = 'Patent Codes:', choices = unique_patent_codes, multiple = TRUE),
         textInput(inputId = 'location', label = 'Location:', value = ''),
         textInput(inputId = 'industry', label = 'Industry:', value = ''),
-        #textAreaInput(inputId = 'submarket_labels_input", label = "submarket labels") ##on David's code
-        actionButton(inputId = 'run_analysis', label = 'Run Analysis', class = 'btn-primary')
+        #textAreaInput(inputId = 'submarket_labels_input', label = "submarket labels", choices = unique_patent_codes, multiple = TRUE), 
+        actionButton(inputId = 'generate_competitive_positioning', label = 'Run Analysis', class = 'btn-primary')
       ),
       column(
         width = 8,
         h4('Results:'),
         textOutput(outputId = 'selected_patent_codes'),
-        plotOutput(outputId = 'competition_plot', height = '500px'),
+        plotOutput(outputId = 'competition_plot', height = '500px'), #for when we add a plot
         h4('Outputs'), 
-        textOutput(outputId = "text_labels_output"),
+        textOutput(outputId = "text_labels_output"), ## from david; this isn't anywhere else in code rn
         DTOutput(outputId = "competition_dt")
       )
     )
@@ -110,7 +115,7 @@ ui <- navbarPage(
       column(
         width = 4,
         h4('Enter your search criteria:'),
-        selectInput(inputId = 'patent_codes', label = 'Patent Codes:', choices = unique_patent_codes, multiple = TRUE),         textInput(inputId = 'location', label = 'Location:', value = ''),
+        selectInput(inputId = 'market_cpcs_input', label = 'Patent Codes:', choices = unique_patent_codes, multiple = TRUE),         textInput(inputId = 'location', label = 'Location:', value = ''),
         textInput(inputId = 'industry', label = 'Industry:', value = ''),
         actionButton(inputId = 'generate_competitive_positioning', label = 'Run Analysis', class = 'btn-primary')
       ),
@@ -142,15 +147,18 @@ server <- function(input,output,session) {
   
   observeEvent(input$generate_competitive_positioning, {
     #select codes
-    selected_codes$competition <- paste("Selected patent codes:", paste(input$patent_codes, collapse = ", "))
-    selected_codes$trends <- paste("Selected patent codes:", paste(input$patent_codes, collapse = ", "))
+    selected_codes$competition <- paste("Selected patent codes:", paste(input$market_cpcs_input, collapse = ", "))
+    selected_codes$trends <- paste("Selected patent codes:", paste(input$market_cpcs_input, collapse = ", "))
     
     #filter the cpc codes
-    dt <- cpc %>%  filter(grepel(pattern = paste(input$market_cpcs_input, sep = "", collapse = "|", x = cpc$cpc_group,ignore.case = T)))
-    #merge with patents
-    dt <- merge(dt, patent, by = "patent_id")
-    #merge with assignee
-    dt <- merge(dt, assignee, by = "patent_id")
+    dt <- cpc %>%  filter(grepl(pattern = paste(input$market_cpcs_input, sep = "", collapse = "|", x = cpc$cpc_group,ignore.case = T))) ## change market_cpcs_input (david) to patent_codes?
+    #bring in smaller merged data 
+    dt <- small_data
+    ## to use when we have larger data
+    # #merge with patents
+    # dt <- merge(dt, patent, by = "patent_id")
+    # #merge with assignee
+    # dt <- merge(dt, assignee, by = "patent_id")
     
 
     ##paste market cpcs...
@@ -158,9 +166,9 @@ server <- function(input,output,session) {
     #copy in other competitive positioning code
     
 
-    competition$dt <- head(dt)
-    competition$dt <- totals
-    ouput$competition_dt <- renderDataTable({competition$dt})
+    competition$dt <- head(dt) #this gives an errors saying competition not found when run outside of the app
+    #competition$dt <- totals ## for when we put in competition trends
+    output$competition_dt <- renderDataTable({competition$dt})
     
   })
   
